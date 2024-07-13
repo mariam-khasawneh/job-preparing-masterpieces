e2d2b47298a66435aec58ed0f0fb0acdb06852a1
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-13 22:13:39 +0300

    Add containers styles

---
+++ my-app/src/Components/styles-components/containers.jsx
@@ -0,0 +1,53 @@
+import styled, { css } from "styled-components";
+
+export const Section = styled.div`
+  display: flex;
+  padding: 64px 96px;
+  background-color: #f7fbfb;
+  font-family: Inter;
+  height: calc(100vbh - 69px);
+
+  @media screen and (min-width: 1024px) {
+    padding: 64px 96px;
+  }
+  /* Media query for screens smaller than 768px */
+  @media (min-width: 768px) and (max-width: 1023px) {
+    padding: 48px;
+  }
+
+  /* Media query for screens smaller than 480px */
+  @media (max-width: 767px) {
+    padding: 32px;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  padding: 64px;
+  border-radius: 16px;
+
+  @media screen and (min-width: 1024px) {
+    padding: 64px 96px;
+  }
+  /* Media query for screens smaller than 768px */
+  @media (min-width: 768px) and (max-width: 1023px) {
+    padding: 48px;
+  }
+
+  /* Media query for screens smaller than 480px */
+  @media (max-width: 767px) {
+    padding: 32px;
+  }
+  /* colors */
+  ${({ dark }) =>
+    dark &&
+    css`
+      background: #081026;
+      color: #dde1ff;
+    `}
+  ${({ light }) =>
+    light &&
+    css`
+      background: #5b4cff;
+    `}
+`;
