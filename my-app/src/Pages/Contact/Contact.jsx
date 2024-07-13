aab4ad430b51980912da1b671eca843f1559880f
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-13 22:43:59 +0300

    Creat Contact Component

--- my-app/src/Pages/Contact/Contact.jsx
+++ my-app/src/Pages/Contact/Contact.jsx
@@ -1,1 +1,11 @@
- 
+import { Section } from "../../Components/styles-components/containers";
+
+function Contact() {
+  return (
+    <Section>
+      <div>Contact Us</div>
+    </Section>
+  );
+}
+
+export default Contact;
