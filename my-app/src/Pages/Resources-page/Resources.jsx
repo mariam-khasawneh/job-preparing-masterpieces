6465678e08f54b1a72f3f7949741d18586781f4d
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-13 22:05:47 +0300

    Add Resources page

---
+++ my-app/src/Pages/Resources-page/Resources.jsx
@@ -0,0 +1,11 @@
+import { Section } from "../../Components/styles-components/containers";
+
+function Resources() {
+  return (
+    <Section>
+      <div>Resources</div>
+    </Section>
+  );
+}
+
+export default Resources;
