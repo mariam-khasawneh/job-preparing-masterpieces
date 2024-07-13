ab7313fea5cd95d76177a8fb9090cfa2a4a2ba84
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-13 22:41:18 +0300

    Creat How It Work functional component

---
+++ my-app/src/Pages/HowItWork/HowItWork.jsx
@@ -0,0 +1,11 @@
+import { Section } from "../../Components/styles-components/containers";
+
+function HowItWork() {
+  return (
+    <Section>
+      <div>How it work</div>
+    </Section>
+  );
+}
+
+export default HowItWork;
