25ef519ccc297fb0f24b9627783f5bbe2a5ee5e1
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-16 22:31:39 +0300

    Add Lesson SVG

---
+++ my-app/src/Components/cards/Course-card/svg/LessonSvg.jsx
@@ -0,0 +1,34 @@
+function LessonsSVG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+    >
+      <g clipPath="url(#clip0_4183_1317)">
+        <path
+          d="M9 4.5V15.75"
+          stroke="#596A95"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+        />
+        <path
+          d="M13.125 2.25C10.875 2.25 9 3.225 9 4.5C9 3.225 7.125 2.25 4.875 2.25C2.625 2.25 0.75 3.225 0.75 4.5V15.75C0.75 14.475 2.625 13.5 4.875 13.5C7.125 13.5 9 14.475 9 15.75C9 14.475 10.875 13.5 13.125 13.5C15.375 13.5 17.25 14.475 17.25 15.75V4.5C17.25 3.225 15.375 2.25 13.125 2.25Z"
+          stroke="#596A95"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          strokeLinecap="square"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_4183_1317">
+          <rect width="18" height="18" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default LessonsSVG;
