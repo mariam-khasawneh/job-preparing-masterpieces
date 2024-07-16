01aa59656bcff9046415bf6e12d2266d24d82245
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-16 21:08:11 +0300

    Create Course component

---
+++ my-app/src/Components/cards/Course-card/CourseCard.jsx
@@ -0,0 +1,11 @@
+import styles from "./CourseCard.module.css";
+function CourseCard() {
+  return (
+    <>
+      <div>Course card</div>
+      <div className={styles.courseImg}></div>
+    </>
+  );
+}
+
+export default CourseCard;
