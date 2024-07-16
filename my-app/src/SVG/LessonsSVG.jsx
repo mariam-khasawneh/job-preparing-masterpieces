df414629a861c79da421efec9142391d4f7f23b2
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-16 21:34:32 +0300

    Add lessons SVG

---
+++ my-app/src/SVG/LessonsSVG.jsx
@@ -0,0 +1,27 @@
+function LessonsSVG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M12 6V21"
+        stroke="#596A95"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M17.5 3C14.5 3 12 4.3 12 6C12 4.3 9.5 3 6.5 3C3.5 3 1 4.3 1 6V21C1 19.3 3.5 18 6.5 18C9.5 18 12 19.3 12 21C12 19.3 14.5 18 17.5 18C20.5 18 23 19.3 23 21V6C23 4.3 20.5 3 17.5 3Z"
+        stroke="#596A95"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="square"
+      />
+    </svg>
+  );
+}
+
+export default LessonsSVG;
