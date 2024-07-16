0792068bee0dba78d820e6b79a36174199d31446
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-16 20:37:54 +0300

    Creat Courses Page

---
+++ my-app/src/Pages/Courses/Courses.jsx
@@ -0,0 +1,11 @@
+import { Section } from "../../Components/styles-components/containers";
+
+function Courses() {
+  return (
+    <Section>
+      <div>Courses Page</div>
+    </Section>
+  );
+}
+
+export default Courses;
