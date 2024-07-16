e9cc2d52f0a3a61a17621b4f422ea5bcac1181d1
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-16 22:31:19 +0300

    Add Student SVG

---
+++ my-app/src/Components/cards/Course-card/svg/StdSVG.jsx
@@ -0,0 +1,35 @@
+function StdSVG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+    >
+      <path
+        d="M9.45775 2.361L15.7898 5.621C16.4028 5.937 16.4028 6.813 15.7898 7.129L9.45775 10.389C9.16975 10.537 8.82875 10.537 8.54175 10.389L2.20975 7.129C1.59675 6.813 1.59675 5.937 2.20975 5.621L8.54175 2.361C8.82975 2.213 9.17075 2.213 9.45775 2.361Z"
+        stroke="#596A95"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M16.2496 6.375C16.0786 7.115 15.9316 8.097 15.9686 9.25C15.9956 10.084 16.1126 10.812 16.2496 11.406"
+        stroke="#596A95"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.25 10.75V14C4.25 15.104 6.377 16 9 16C11.623 16 13.75 15.104 13.75 14V10.75"
+        stroke="#596A95"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default StdSVG;
