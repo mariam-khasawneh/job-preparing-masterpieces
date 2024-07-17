94b1185587a4aeb3fc4106837f9ca0164c1e997d
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-18 01:07:42 +0300

    Add rating svg

---
+++ my-app/src/Components/cards/Coach-card/svg/RatingSvg.jsx
@@ -0,0 +1,25 @@
+function RatingSvg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="none"
+    >
+      <g clipPath="url(#clip0_4185_2619)">
+        <path
+          d="M14.991 5.43344L9.81539 4.68156L7.50039 -0.0078125L5.18539 4.68156L0.00976562 5.43344L3.75539 9.08469L2.87102 14.2391L7.50039 11.8066L12.1298 14.2391L11.2454 9.08469L14.991 5.43344Z"
+          fill="#F4BD52"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_4185_2619">
+          <rect width="15" height="15" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default RatingSvg;
