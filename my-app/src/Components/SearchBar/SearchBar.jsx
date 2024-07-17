d97fc910686bbb5d57ab251fb216e1ca4ad3c5e4
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-18 01:07:20 +0300

    Create search bar

---
+++ my-app/src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,13 @@
+function SearchBar() {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search Courses"
+        className="border-2 border-[#dae4f1] rounded-lg flex p-3 items-end gap-3 bg-none"
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
