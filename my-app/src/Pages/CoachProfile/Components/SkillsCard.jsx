7778cfa954e5add0b9112c7c22fd7a781b74be6f
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-18 01:34:19 +0300

    Create skill card component

---
+++ my-app/src/Pages/CoachProfile/Components/SkillsCard.jsx
@@ -0,0 +1,20 @@
+import { H6 } from "../../../Components/Typography-components/Typography";
+import Badges from "../../../Components/Badges/Badges";
+
+function SkillsCard() {
+  return (
+    <div className="flex flex-col p-8 items-start gap-5 rounded-2xl border-2	 border-slate-200  ">
+      <H6 className="font-semibold text-slate-900 leading-5	">Skills</H6>
+      <div className="flex gap-2 flex-wrap">
+        <Badges>Active Listening</Badges>
+        <Badges>Empathy</Badges>
+        <Badges>Communication</Badges>
+        <Badges>Active Listening</Badges>
+        <Badges>Communication</Badges>
+        <Badges>Motivational Techniques</Badges>
+      </div>
+    </div>
+  );
+}
+
+export default SkillsCard;
