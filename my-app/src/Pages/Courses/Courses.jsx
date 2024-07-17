b65477fd11cde524bc14e5229370a2c13c517390
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-18 01:09:57 +0300

    Add courses card to courses page

--- my-app/src/Pages/Courses/Courses.jsx
+++ my-app/src/Pages/Courses/Courses.jsx
@@ -1,9 +1,53 @@
 import { Section } from "../../Components/styles-components/containers";
+import { H5 } from "../../Components/Typography-components/Typography";
+import SearchBar from "../../Components/SearchBar/SearchBar";
+import CourseCard from "../../Components/cards/Course-card/CourseCard";
 
 function Courses() {
   return (
-    <Section>
-      <div>Courses Page</div>
+    <Section className="flex flex-col">
+      <div className="flex flex-col sm:flex-col lg:flex-row justify-between self-stretch	">
+        <H5 className="text-blue-900 font-inter font-semibold"> Our Courses</H5>
+        <SearchBar />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  sm:ga-4 gap-6 pt-12">
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+      </div>
     </Section>
   );
 }
