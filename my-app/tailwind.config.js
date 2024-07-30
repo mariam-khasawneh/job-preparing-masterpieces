449ee3b7d69415fc0e7edc70c789ba164643beb8
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-30 23:46:48 +0300

    Tailwind coustom colors

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryIndigo: "#655DE9",
+        hoverIndigo: "#544BE7",
+        primaryYellow: "#FEF08A",
+        hoverYellow: "FEEC67",
+      },
+      fontFamily: { spaceGrotesk: ['"Space Grotesk"', "sans-serif"] },
+    },
   },
   plugins: [],
 };
