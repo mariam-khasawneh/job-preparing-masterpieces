2eb0df7993ef6fee1476dad613b317bb7e31dcad
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-30 23:47:12 +0300

    Seachbar component

--- my-app/src/Components/SearchBar/SearchBar.jsx
+++ my-app/src/Components/SearchBar/SearchBar.jsx
@@ -1,10 +1,10 @@
 function SearchBar() {
   return (
-    <div>
+    <div className="w-full">
       <input
         type="text"
         placeholder="Search Courses"
-        className="border-2 border-[#dae4f1] rounded-lg flex p-3 items-end gap-3 bg-none"
+        className="border-2 border-[#dae4f1] rounded-lg flex p-3 items-end gap-3 bg-none w-full"
       />
     </div>
   );
