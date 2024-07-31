9c89b9471e18e04924f1ff2103f37f8b887b8c2f
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-31 11:43:42 +0300

    sidbar

--- my-app/src/Dashboard/Components/Sidebar.jsx
+++ my-app/src/Dashboard/Components/Sidebar.jsx
@@ -1,5 +1,25 @@
+import React from "react";
+
+//icons
+
+import { Box, MessageSquare, Calendar, User, Users } from "react-feather";
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const SIDEBAR_LINKS = [
+    { id: 1, path: "/", name: "Dashboard", icon: Box },
+    { id: 2, path: "/members", name: "Members", icon: Users },
+    { id: 3, path: "/messages", name: "Messages", icon: MessageSquare },
+    { id: 5, path: "/", name: "Dashboard", icon: Box },
+    { id: 6, path: "/", name: "Dashboard", icon: Box },
+  ];
+
+  return (
+    <div>
+      {/* logo */}
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
