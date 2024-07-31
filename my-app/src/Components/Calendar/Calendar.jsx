97d7a8fbd577950e237369d7a3e76fcd39a6f5c7
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-31 22:55:05 +0300

    Calendar Component

---
+++ my-app/src/Components/Calendar/Calendar.jsx
@@ -0,0 +1,11 @@
+import CalendarComponent from "./StyledCalendar ";
+function Calendar() {
+  return (
+    <div className="w-full">
+      <h1>Custom Styled Calendar</h1>
+      <CalendarComponent />
+    </div>
+  );
+}
+
+export default Calendar;
