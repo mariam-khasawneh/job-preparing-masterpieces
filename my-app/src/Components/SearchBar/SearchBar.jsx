b78b08bd85df2b5195d3d0f8cb8bf40f485fcd41
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-31 22:17:50 +0300

    Edit searchbar

--- my-app/src/Components/SearchBar/SearchBar.jsx
+++ my-app/src/Components/SearchBar/SearchBar.jsx
@@ -1,10 +1,10 @@
-function SearchBar() {
+function SearchBar({ placeholder }) {
   return (
     <div className="w-full">
       <input
         type="text"
-        placeholder="Search Courses"
-        className="border-2 border-[#dae4f1] rounded-lg flex p-3 items-end gap-3 bg-none w-full"
+        placeholder={placeholder}
+        className="border-2 border-[#dae4f1] rounded-lg flex p-3 items-end gap-3 bg-none w-full h-10 focus:outline-none focus:border-primaryIndigo"
       />
     </div>
   );
