daeaedc7396335d4fc95652af666c715732b4b52
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-09-16 14:00:14 +0300

    Recover authSlice

--- Website/Frontend/src/Store/authSlice.js
+++ Website/Frontend/src/Store/authSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import Cookies from "js-cookie";
 
 const authSlice = createSlice({
   name: "auth",
@@ -10,18 +9,15 @@ const authSlice = createSlice({
   reducers: {
     login: (state /* , action*/) => {
       state.isLoggedIn = true;
-      // state.user = action.payload;
     },
     logout: (state) => {
       state.isLoggedIn = false;
-      Cookies.remove("token");
     },
     checkAuthState: (state) => {
       const authState = localStorage.getItem("authState");
       if (authState) {
         const { auth } = JSON.parse(authState);
         state.isLoggedIn = auth.isLoggedIn;
-        // state.user = auth.user;
       }
     },
   },
