d4567264f8fb5e3fe88040c0673a88f03d744970
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-09-16 14:06:00 +0300

    Email handel

---
+++ Website/Backend/Utils/emailUtils.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (to, subject, text) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to,
+      subject,
+      text,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+    return info;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Failed to send email. Please try again.");
+  }
+};
+
+module.exports = { sendEmail };
